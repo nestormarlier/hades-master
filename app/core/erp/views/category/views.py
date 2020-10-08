c0067671c13910cea7897fd973b1ff4228bc0834
nestormarlier <nestor.mar@gmail.com>
2020-10-07 22:31:06 -0300

    @method_decorator with csrf_exempt

--- app/core/erp/views/category/views.py
+++ app/core/erp/views/category/views.py
@@ -1,10 +1,10 @@
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
-from django.shortcuts import render, redirect
-from django.views.generic import ListView
-
 from core.erp.models import Category
-
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import ListView
+from django.http import JsonResponse
 
 def category_list(request):
     data = {
@@ -18,9 +18,13 @@ class CategoryListView(ListView):
     model = Category
     template_name = 'category/list.html'
 
-    @method_decorator(login_required)
+    @method_decorator(csrf_exempt)
     def dispatch(self,request, *args, **kwargs):
         return super().dispatch(request,*args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        data = {'nombre': 'Nestor'}
+        return JsonResponse(data)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
